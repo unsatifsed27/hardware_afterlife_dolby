73b45957a93437f0b16294e437febf997f452a27
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-09-04 03:58:43 +0530

    AfterEcho: DolbyManager: Add intelligent equalizer setting
    Move preference-related classes to a new package while we're at it,
    to reduce code clutter.
    Also fix graphical equlizer
    
    Change-Id: I2430e8ab9b6758503ce1777ec985a3e400b55b8e
    Signed-off-by: unsatifsed27 <unsatifsederror@gmail.com>

--- DolbyManager/src/com/afterlifeos/echo/after/DolbyActivity.kt
+++ DolbyManager/src/com/afterlifeos/echo/after/DolbyActivity.kt
@@ -7,6 +7,7 @@
 package com.afterlifeos.echo.after
 
 import android.os.Bundle
+import com.afterlifeos.echo.after.preference.DolbySettingsFragment
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity
 
 private const val TAG = "DolbyActivity"
